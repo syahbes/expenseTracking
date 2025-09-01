8f192047b6bf164f540f16b11c5dcc2059e5be52
Shlomo Y <shlomo@techtio.io>
2025-09-01 16:12:49 +0300

    space

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,11 +1,11 @@
 const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorDark = '#f2f2f2';
 
 export const Colors = {
   light: {
     text: '#11181C',
-    background: '#fff',
-    cardBackgroundColor: '#F2F4F7',
+    background: '#F8FAFC',
+    cardBackgroundColor: '#EEF1F5',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
@@ -13,11 +13,14 @@ export const Colors = {
   },
   dark: {
     text: '#ECEDEE',
-    background: '#171422',
-    cardBackgroundColor: '#242231',
+    background: '#232634',
+    cardBackgroundColor: '#3C3C48',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
   },
 };
+
+// background: '#171422',
+// cardBackgroundColor: '#242231',
